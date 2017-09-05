8f8e650fd38ed79ca496c5d745c8b9c72e75374f
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2017-09-05 07:37:21 -0300

    Organização na pasta public, reescrita do gerenciar_professores.php, adicionanto um menu basico

--- App/Administracao/gerenciador_professores.php
+++ App/Administracao/gerenciador_professores.php
@@ -1,229 +1,154 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CinDB</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>CinDB</title>
+      <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
+      <link rel="stylesheet" href="../../public/css/mgmtpgs_style.css">
+      <link rel="stylesheet" href="../../public/css/ie10-viewport-bug-workaround.css">
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <!-- Custom Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Aldrich|Fugaz+One|Quantico" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="../../public/CSS/style.css" rel="stylesheet">
-    <link href="../../public/CSS/tagsinput.css" rel="stylesheet">
+      <!-- temp -->
+      <style media="screen">
+      .wrapper{
+            display: flex;
+      }
 
+      .sidebar {
+            background-color: #f1f1f1;
+            border-right: 1px solid #c1c1c1;
+            height: 100vh;
+            margin-right: 25px;
+      }
+
+      .sidebar ul{
+            list-style: none;
+            padding: 0;
+            background: #2277ff;
+            border: 1px solid #1155aa;
+            border-radius: 4px;
+      }
+
+
+      .sidebar ul li a{
+            border-radius: 4px;
+            padding: 5px 10px;
+            display: block;
+            color: #ffffff;
+            text-decoration: none;
+            font-weight: bold;
+      }
+
+      .sidebar ul li.active a{
+            //border: 1px solid #885500;
+            background: #ff9900;
+      }
+      
+
+      .sidebar ul li:not(.active) a:hover{
+            //border: 1px solid #2277ff;
+            border-radius: 0;
+            background: #1155aa;
+      }
+      </style>
+      <!-- temp -->
 </head>
 <body>
-    <h2>Gerenciador de Professores</h2>
-    <hr>
+      <div class="container-fluid">
+            <div class="wrapper row">
+                  <!-- Inicio nav -->
+                  <nav class="sidebar col-md-2">
+                        <!-- Nav Header -->
+                        <div class="sidebar-header">
+                              <h2>CinDB</h2>
+                        </div>
+                        <!-- Nav Links -->
+                        <ul>
+                              <li class="active"><a href="#">Gerenciar Professores</a></li>
+                              <li><a href="#">Gerenciar Cadeiras</a></li>
+                              <li><a href="#">Exercicios</a></li>
+                        </ul>
+                  </nav>
+                  <!-- Fim nav -->
+                  <div class="col-md-10">
+                        <h3>Gerenciador de Professores</h2>
+                              <!-- modal trigger-->
+                              <button type="button" class="btn btn-info btn-default" data-toggle="modal" data-target="#modalCadastro">Adicionar Novo</button>
+                              <hr>
+                              <table class="table table-bordered">
+                                    <thead>
+                                          <tr>
+                                                <th>Nome</th>
+                                                <th>Cadeiras</th>
+                                                <th>Opções</th>
+                                          </tr>
+                                    </thead>
+                                    <tbody>
+                                          <tr>
+                                                <td>col</td>
+                                                <td>col</td>
+                                                <td>col</td>
+                                          </tr>
+                                          <tr>
+                                                <td>col</td>
+                                                <td>col</td>
+                                                <td>col</td>
+                                          </tr>
+                                          <tr>
+                                                <td>col</td>
+                                                <td>col</td>
+                                                <td>col</td>
+                                          </tr>
+                                    </tbody>
+                              </table>
 
-        <!-- modal trigger-->
-    <button type="button" class="btn btn-info btn-default" data-toggle="modal" data-target="#modalCadastro">Adicionar Novo</button>
-    
-    <!-- aqui vai ficar a lista de cadeiras ja existentes com um botao de "edit" do lado-->  
-    <table class="table-bordered">
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Cadeiras</th>
-                <th>Opções</th>
-            </tr>
-        </thead>
-        <tbody>
-        </tbody>
+                        </div>
+                  </div>
+            </div>
 
-        <!--?php
-            require_once "../Core/dbconnection.php";
 
-            $conn = dbconnection::conn();
-            
-            $result = $conn->query("SELECT * FROM cadeiras");
+            <!-- Modal -->
+            <div id="modalCadastro" class="modal fade" role="dialog">
+                  <div class="modal-dialog">
 
-            foreach ($result as $row) {
-                echo"<tr>";
-                echo"<td>$row[idprofessores]</td>";
-                echo"<td>$row[nome]</td>";
-                echo"<td> vai ter cadeiras aqui <td>";
-                echo"<td><a href='#'>Editar</a><td>";
-                echo"<td><a href='#'>Excluir</a><td>";
-                echo "</tr>";
-            }
-     
-        ?-->
-    </table>
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                              <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Cadastrar Professor</h4>
+                              </div>
+                              <div class="modal-body">
+                                    <!--form para cadastro de novas cadeiras-->
+                                    <form method="post" name="novo-professor" action="../Core/form_cadeiras_tags.php">
+                                          <div class="form-group">
+                                                <label for="nome" data-toggle="tooltip" title="Insira o nome do professor">Nome:</label>
+                                                <input class="form-control" type="text" name="nome" id="nome">
+                                          </div>
 
-    <!-- Modal -->
-    <div id="modalCadastro" class="modal fade" role="dialog">
-      <div class="modal-dialog">
-
-        <!-- Modal content-->
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Cadastrar Professor</h4>
-          </div>
-          <div class="modal-body">
-            <!--form para cadastro de novas cadeiras-->
-            <form method="post" name="novo-professor" action="../Core/form_cadeiras_tags.php">
-                <div class="form-group">
-                    <label for="nome" data-toggle="tooltip" title="Insira o nome do professor">Nome:</label>
-                    <input class="form-control" type="text" name="nome" id="nome">
-                </div>
-
-                <strong data-toggle="tooltip" title="Selecione as cadeiras que este professor leciona.">Cadeiras:</strong>
-                <div class="form-group" id="cadeiras">                    
-                    <label class="checkbox-inline">
-                        <input type="checkbox">cadeira1
-                    </label>
-                    <label class="checkbox-inline">
-                        <input type="checkbox">cadeira2
-                    </label>
-                    <label class="checkbox-inline">
-                        <input type="checkbox">cadeira3
-                    </label>
-                </div>
-                <button type="submit" class="btn btn-success">Cadastrar</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-
-<!--
-    <div class="row">
-        <div class="col-lg-6">
-
-            <form role="form">
-
-                <div class="form-group">
-                    <label>Text Input</label>
-                    <input class="form-control">
-                    <p class="help-block">Example block-level help text here.</p>
-                </div>
-
-                <div class="form-group">
-                    <label>Text Input with Placeholder</label>
-                    <input class="form-control" placeholder="Enter text">
-                </div>
-
-                <div class="form-group">
-                    <label>Static Control</label>
-                    <p class="form-control-static">email@example.com</p>
-                </div>
-
-                <div class="form-group">
-                    <label>File input</label>
-                    <input type="file">
-                </div>
-
-                <div class="form-group">
-                    <label>Text area</label>
-                    <textarea class="form-control" rows="3"></textarea>
-                </div>
-
-                <div class="form-group">
-                    <label>Checkboxes</label>
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" value="">Checkbox 1
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" value="">Checkbox 2
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" value="">Checkbox 3
-                        </label>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label>Inline Checkboxes</label>
-                    <label class="checkbox-inline">
-                        <input type="checkbox">1
-                    </label>
-                    <label class="checkbox-inline">
-                        <input type="checkbox">2
-                    </label>
-                    <label class="checkbox-inline">
-                        <input type="checkbox">3
-                    </label>
-                </div>
-
-                <div class="form-group">
-                    <label>Radio Buttons</label>
-                    <div class="radio">
-                        <label>
-                            <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Radio 1
-                        </label>
-                    </div>
-                    <div class="radio">
-                        <label>
-                            <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Radio 2
-                        </label>
-                    </div>
-                    <div class="radio">
-                        <label>
-                            <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Radio 3
-                        </label>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label>Inline Radio Buttons</label>
-                    <label class="radio-inline">
-                        <input type="radio" name="optionsRadiosInline" id="optionsRadiosInline1" value="option1" checked>1
-                    </label>
-                    <label class="radio-inline">
-                        <input type="radio" name="optionsRadiosInline" id="optionsRadiosInline2" value="option2">2
-                    </label>
-                    <label class="radio-inline">
-                        <input type="radio" name="optionsRadiosInline" id="optionsRadiosInline3" value="option3">3
-                    </label>
-                </div>
-
-                <div class="form-group">
-                    <label>Selects</label>
-                    <select class="form-control">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label>Multiple Selects</label>
-                    <select multiple class="form-control">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div>
-
-                <button type="submit" class="btn btn-default">Submit Button</button>
-                <button type="reset" class="btn btn-default">Reset Button</button>
-
-            </form>
-
-        </div>
-    </div>
--->
-    
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="../../public/js/tagsinput.js"></script>
-    <script>
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();   
-        });
-    </script>
-</body>
-</html>
+                                          <strong data-toggle="tooltip" title="Selecione as cadeiras que este professor leciona.">Cadeiras:</strong>
+                                          <div class="form-group" id="cadeiras">
+                                                <label class="checkbox-inline">
+                                                      <input type="checkbox">cadeira1
+                                                </label>
+                                                <label class="checkbox-inline">
+                                                      <input type="checkbox">cadeira2
+                                                </label>
+                                                <label class="checkbox-inline">
+                                                      <input type="checkbox">cadeira3
+                                                </label>
+                                          </div>
+                                          <button type="submit" class="btn btn-success">Cadastrar</button>
+                                    </form>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <script type="text/javascript" src="../../public/js/bootstrap.min.js"></script>
+            <script>
+            $(document).ready(function(){
+                  $('[data-toggle="tooltip"]').tooltip();
+            });
+            </script>
+      </body>
+      </html>
