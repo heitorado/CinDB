e8eb3b1b6a8ea33cac075030f848a3c41dd55e52
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2017-09-13 13:11:46 -0300

    comentarios

--- Core/ProfessoresControle.php
+++ Core/ProfessoresControle.php
@@ -2,7 +2,7 @@
 // CLASSE RESPONSAVEL COM GERENCIAR OS PROCESSOS DA PAGINA "gerenciador_professores.php"
 
 require_once "db/Database.php";
-require_once "Template.php";
+require_once "Template.php"; // PROVAVELMENTE VAI SER APAGADA
 
 // CASO A CONDIÇÃO SEJA VERDADEIRA, ELE REALIZA O INSERT
 if (isset($_POST['nome']) && isset($_POST['cadastrar'])) {
@@ -10,11 +10,13 @@ if (isset($_POST['nome']) && isset($_POST['cadastrar'])) {
       $professoresControle->insertProfessores();
 }
 
+// CASO A CONDIÇÃO SEJA VERDADEIRA, ELE REALIZA O UPDATE
 if (isset($_POST['nome']) && isset($_POST['modificar'])) {
       $professoresControle = new ProfessoresControle();
       $professoresControle->updateProfessor();
 }
 
+// CASO A CONDIÇÃO SEJA VERDADEIRA, ELE REALIZA A BUSCA E IMPRIME O JSON
 if (isset($_POST['buscarProfessor'])) {
       $professoresControle = new ProfessoresControle();
       echo $professoresControle->getProfessor();
@@ -32,7 +34,7 @@ class ProfessoresControle
       public $nome;
       public $status;
 
-      // CRIA A INSTANCIA DA CLASSE DbFuncoes PASSANDO A TABELA QUE VAI MANIPULAR.
+      // CRIA A INSTANCIA DA CLASSE Database PASSANDO A TABELA QUE VAI MANIPULAR.
       function __construct()
       {
             $this->dbFun = new Database("cindb","localhost", "root", "Leozinho580");
@@ -49,14 +51,15 @@ class ProfessoresControle
                         array_push($cadeiras,$check);
                   }
             }
-            $this->dbFun->transaction();
+            $this->dbFun->transaction(); // INICIA A TRANSAÇÃO
 
             // FILTRA O QUE FOI DIGITADO RETIRANDO TAGS DO CARACTER
             $this->nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+            
             $query = "INSERT INTO professores (nome) VALUES (:nome)";
-
             $this->status = $this->dbFun->insert($query,[":nome" => $this->nome]);
 
+            // CASO NÃO DEU ERRO AO INSERIR E EXISTE CADEIRAS
             if (sizeof($cadeiras) > 0 && $this->status)
             {
                   $this->idprofessor = $this->dbFun->lastInsertId();
@@ -73,14 +76,15 @@ class ProfessoresControle
 
             // REDIRECIONA INDICANDO SUCESSO OU FALHA
             if (!$this->status) {
-                  $this->dbFun->rollBack();
+                  $this->dbFun->rollBack(); // SALVA MUDANÇAS
                   header("Location:../App/Administracao/gerenciador_professores.php?insertStatus=false");
             }else {
-                  $this->dbFun->commit();
+                  $this->dbFun->commit(); // CANCELA MUDANÇAS
                   header("Location:../App/Administracao/gerenciador_professores.php?insertStatus=true");
             }
       }
-
+      
+      // ALTERA DADOS DE UM PROFESSOR
       public function updateProfessor()
       {
             // PEGA AS CADEIRAS SELECIONADAS
@@ -98,13 +102,16 @@ class ProfessoresControle
             $this->idprofessor = filter_input(INPUT_POST, 'idprofessor', FILTER_SANITIZE_STRING);
 
             $query = "UPDATE professores SET nome = :nome WHERE idprofessor = :idprofessor";
-
             $this->status = $this->dbFun->update($query,[":nome" => $this->nome, ":idprofessor" => $this->idprofessor]);
+            
+            // CASO A MUDANÇA FOI REALIZADA COM SUCESSO
             if ($this->status) {
+                  // APAGA O RELACIONAMENTO JA CADASTRADO
                   $query = "DELETE FROM professores_has_cadeiras WHERE professores_idprofessor = :idprofessor";
                   $this->status = $this->dbFun->delete($query, [":idprofessor" => $this->idprofessor]);
             }
-
+            
+            // CASO NÃO DEU ERRO AO DELETAR E EXISTE CADEIRAS
             if (sizeof($cadeiras) > 0 && $this->status)
             {
                   $query = "INSERT INTO professores_has_cadeiras VALUES (?,?)";
@@ -194,6 +201,8 @@ class ProfessoresControle
       // CARREGA A PARTE SUPERIOR DO TEMPLATE
       public function renderHeader()
       {
+            // PROVAVELMENTE IREI REMOVER A CLASSE Template E ALTERAR PARA UM -> REQUIRE "../../App/Administracao/_templateHeader.php";
+            // MOTIVO, CODIGO PHP NAO RODA DESSA FORMA
             $template = new Template("../../App/Administracao/_templateHeader.php");
             $template->set("path", "../..");
             $template->set("title", "CinDB");
@@ -203,6 +212,8 @@ class ProfessoresControle
       // CARREGA A PARTE INFERIOR DO TEMPLATE
       public function renderFooter()
       {
+            // PROVAVELMENTE IREI REMOVER A CLASSE Template E ALTERAR PARA UM -> REQUIRE "../../App/Administracao/_templateHeader.php";
+            // MOTIVO, CODIGO PHP NAO RODA DESSA FORMA
             $template = new Template("../../App/Administracao/_templateFooter.php");
             $template->set("path", "../..");
             echo $template->output();
