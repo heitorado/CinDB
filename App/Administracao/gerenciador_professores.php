2ae023235ab352ad280bb8c86141b53b85c7580c
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2017-09-13 11:43:31 -0300

    Comentarios

--- App/Administracao/gerenciador_professores.php
+++ App/Administracao/gerenciador_professores.php
@@ -1,9 +1,11 @@
 <?php
-require_once "../../Core/ProfessoresControle.php";
-$status = null;
-$professoresControle = new ProfessoresControle();
-$professoresControle->renderHeader();
+require_once "../../Core/ProfessoresControle.php"; // Carregando a classe de controle
+$status = null; // Variavel para mensagens após interação com o BD
 
+$professoresControle = new ProfessoresControle();  // Objeto do controle
+$professoresControle->renderHeader(); // Carrega o html superior padrão
+
+// VERIFICA SE VEIO UM 'GET'. CASO POSITIVO É COLOCADO UMA MENSGEM NO $status.
 if (!is_null(filter_input(INPUT_GET, 'insertStatus'))) {
       $status = filter_input(INPUT_GET, 'insertStatus') == "true" ? "Professor cadastrado" : "Erro ao cadastrar";
 }
@@ -11,17 +13,17 @@ elseif (!is_null(filter_input(INPUT_GET, 'deleteStatus'))) {
       $status = filter_input(INPUT_GET, 'deleteStatus') == "true" ? "Professor removido" : "Erro ao remover";
 }
 elseif (!is_null(filter_input(INPUT_GET, 'updateStatus'))) {
-      $status = filter_input(INPUT_GET, 'updateStatus') == "true" ? "Professor modicado" : "Erro ao modificar";
+      $status = filter_input(INPUT_GET, 'updateStatus') == "true" ? "Professor modificado" : "Erro ao modificar";
 }
 
-$professores = $professoresControle->getProfessores();
-$cadeiras = $professoresControle->getCadeiras();
+$professores = $professoresControle->getProfessores(); // CARREGA TODOS OS PROFESSORES
+$cadeiras = $professoresControle->getCadeiras(); // CARREGA TODAS AS CADEIRAS PARA O FORMULARIO
 ?>
 
 <h3>Gerenciador de Professores</h3>
 <!-- modal trigger-->
 <h4><?=$status ?></h4>
-<button type="button" class="btn btn-info btn-default" id="insert" data-toggle="modal" data-target="#modalCadastro">Adicionar Novo</button>
+<button type="button" class="btn btn-info btn-default" id="insert" data-toggle="modal" data-target="#modalProfessor">Adicionar Novo</button>
 <hr>
 <table class="table table-bordered">
       <thead>
@@ -32,13 +34,14 @@ $cadeiras = $professoresControle->getCadeiras();
             </tr>
       </thead>
       <tbody>
+            <!-- EXIBE OS DADOS -->
             <?php foreach ($professores as $key => $value) : ?>
                   <tr>
                         <td><?=$value['nome'] ?></td>
                         <td><?=$value['cadeiras']?></td>
                         <td>
                               <a href="../../Core/ProfessoresControle.php?delete=<?=$value['idprofessor']?>">Remover</a>
-                              <a href="#" id="update-<?=$value['idprofessor']?>" data-toggle="modal" data-target="#modalCadastro">Alterar</a>
+                              <a href="#" id="update-<?=$value['idprofessor']?>" data-toggle="modal" data-target="#modalProfessor">Alterar</a>
                         </td>
                   </tr>
             <?php endforeach; ?>
@@ -46,7 +49,7 @@ $cadeiras = $professoresControle->getCadeiras();
 </table>
 
 <!-- Modal -->
-<div id="modalCadastro" class="modal fade" role="dialog">
+<div id="modalProfessor" class="modal fade" role="dialog">
       <div class="modal-dialog">
 
             <!-- Modal content-->
@@ -73,19 +76,20 @@ $cadeiras = $professoresControle->getCadeiras();
                                     <?php endforeach; ?>
                               </div>
                               <div id="submit">
-
+                                    <!-- ATRAVES DO JQUERY, SERA ADICIONADO O BOTÃO CORRETO -->
                               </div>
-                              <!--input type="submit" name="cadastrar" value="cadastrar" class="btn btn-success"-->
                         </form>
                   </div>
             </div>
       </div>
 </div>
 <script type="text/javascript">
+//INSERE O BOTAO DE CADASTRO
 $("#insert").click(function(){
       limpaModal();
       $("#submit").html('<input type="submit" name="cadastrar" value="cadastrar" class="btn btn-success">');
 });
+// INSERE O BOTAO DE MODIFICAR E CARREGA OS DADOS ATUAIS
 $( "a[id^='update']").click(function(){
       limpaModal();
       $("#submit").html('<input type="submit" name="modificar" value="modificar" class="btn btn-default">');
@@ -95,6 +99,7 @@ $( "a[id^='update']").click(function(){
       buscaProfessor(id);
 });
 
+// BUSCA OS DADOS DO PROFESSOR
 function buscaProfessor(id) {
       $.ajax({
             type:"POST",
@@ -120,6 +125,7 @@ function buscaProfessor(id) {
       });
 }
 
+// LIMPA O MODAL
 function limpaModal() {
       $("#nome").val("");
       $("#idprofessor").val("");
@@ -129,4 +135,4 @@ function limpaModal() {
 }
 
 </script>
-<?php $professoresControle->renderFooter(); ?>
+<?php $professoresControle->renderFooter(); //INSERE A PARTE INFERIOR PADRAO ?>
